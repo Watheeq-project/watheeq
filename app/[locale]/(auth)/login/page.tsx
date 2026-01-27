e3057773ed72d05608a3c31b6fcdca5a796ef985
moemad <moemad504@gmail.com>
2026-01-28 00:19:56 +0200

    feat: setup next-intl with zod and react-hook-form

--- app/[locale]/(auth)/login/page.tsx
+++ app/[locale]/(auth)/login/page.tsx
@@ -1,9 +1,11 @@
+import {useTranslations} from "next-intl";
 
 
 export default function Home() {
+    const t = useTranslations("login")
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        login
+        {t("title")}
     </div>
   );
 }
